a10b50acb7140b66b2dea51ec21e47bcbba43a96
Andarwira051 <114243094+Andarwira051@users.noreply.github.com>
2022-12-20 01:02:48 +0800

    Add files via upload

--- SIAK21A/src/id/ac/undiksha/siak/entities/Pegawai.java
+++ SIAK21A/src/id/ac/undiksha/siak/entities/Pegawai.java
@@ -1,44 +1,47 @@
 package id.ac.undiksha.siak.entities;
 
-public class Pegawai {
-
-	private String nama;
-	private String alamat;
-	private boolean jeniskelamin;// 0 perempuan, 1 laki-laki
+public class Pegawai extends Manusia {
 	private String nip;
 	private String unit;
 	
-	public String getNama() {
-		return nama;
+	public Pegawai() {
+		super();
+		this.setNip("(NIP belum diisi)");
+		this.setUnit("(Unit belum diisi)");	
+		
 	}
-	public void setNama(String nama) {
-		this.nama = nama;
+	
+	public Pegawai(String nama, String alamat, boolean jenisKelamin, String nip, String unit) {
+		super(nama, alamat, jenisKelamin);
+		this.nip = nip;
+		this.unit = unit;
 	}
-	public String getAlamat() {
-		return alamat;
+
+	public void printAllInfo() {
+		System.out.println("Nama: "+ this.getNama());
+		System.out.println("NIM: "+ this.getNip());
+		System.out.println("Alamat: "+ this.getAlamat());
+		System.out.println("Unit: "+ this.getUnit());
+		System.out.println("Jenis Kelamin: " + (this.getJenisKelamin() ? "Laki-laki" : "Perempuan"));
 	}
-	public void setAlamat(String alamat) {
-		this.alamat = alamat;
-	}
-	public boolean isJeniskelamin() {
-		return jeniskelamin;
-	}
-	public void setJeniskelamin(boolean jeniskelamin) {
-		this.jeniskelamin = jeniskelamin;
-	}
+
 	public String getNip() {
 		return nip;
 	}
+
 	public void setNip(String nip) {
 		this.nip = nip;
 	}
+
 	public String getUnit() {
 		return unit;
 	}
+
 	public void setUnit(String unit) {
 		this.unit = unit;
 	}
 
+	
 	
 	
 }
